e09605af64d5033f763183a90f7e197dacbd3b1e
D4rkli <anzhelikakosolapova@gmail.com>
2026-02-01 04:56:55 +0300

    Initial deploy

--- backend/app/config.py
+++ backend/app/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     BOT_TOKEN: str
 
     BASE_URL: str = "https://d4rkli.ru"
-    WEBAPP_URL: str = "//d4rkli.ru/webapp/"
+    WEBAPP_URL: str = "//d4rkli.ru/webapp/?"
     SQLITE_PATH: str = "sqlite+aiosqlite:///./dnd._v2.sqlite3"
     DM_USER_IDS: str = ""
 
